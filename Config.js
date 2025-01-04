f57a32eff02547d90a7cf404e9d4428aad31a43d
akshat jyoti <90604893+akshew@users.noreply.github.com>
2025-01-04 18:36:47 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,60 @@
+const { ButtonStyle } = require("discord.js");
+module.exports = {
+    Token: "",
+    Prefix: ".",
+    Client: {
+        ID: "",
+        Secret: "",
+    },
+    button: {
+        grey: ButtonStyle.Secondary,
+        blue: ButtonStyle.Primary,
+        link: ButtonStyle.Link,
+        red: ButtonStyle.Danger,
+        green: ButtonStyle.Success,
+    },
+    spotify: {
+        ID: "",
+        Secret: "",
+    },
+    Api: {
+        Topgg: "",
+    },
+    MongoData: "",
+    EmbedColor: "",
+    Owners: [""],
+    Nodes: [
+        {
+            name: "",
+            url: "",
+            auth: "",
+            secure: false,
+        },
+        {
+            name: "",
+            url: "",
+            auth: "",
+            secure: false,
+        },
+        {
+            name: "",
+            url: "",
+            auth: "",
+            secure: false,
+        },
+    ],
+    hooks: {
+        guildAdd: "",
+        guildRemove: "",
+        Error: "",
+    },
+    links: {
+        invite: "",
+        bg: "",
+        support: "",
+        vote: "",
+        banner: "",
+        spotify: "",
+        soundcloud: "",
+    },
+};
